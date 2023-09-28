23a6e18c61ff784d37ba9afd3372b47adacb7b6f
Atul Jadhav <prathik.paip_ext@ltts.com>
2023-09-28 15:03:23 +0530

    project card css added

--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,45 +1,42 @@
 import React from "react";
 import { Box } from "@mui/material";
 import data from "../data/index.json";
-import { PageHeadingText } from "./styles";
+import {
+  PageHeadingText,
+  PoppinsEighteenText,
+  PoppinsForteenText,
+  PoppinsSixteenText,
+} from "./styles";
 import { MyWorkText } from "./constants/Text";
 
 export default function MyPortfolio() {
   return (
     <Box id="projects">
-      <PageHeadingText sx={{color:"#003C2F"}}>{MyWorkText}</PageHeadingText>
+      <PageHeadingText sx={{ color: "#003C2F" }}>{MyWorkText}</PageHeadingText>
       <Box className="portfolio--section">
-        <Box className="portfolio--section--container ">
+        <Box className="portfolio--section--container">
           {data?.portfolio?.map((item, index) => (
-            <div key={item.id} className="portfolio--section--card">
-              <div className="portfolio--section--img">
+            <Box key={item.id} className="portfolio--section--card">
+              <Box className="portfolio--section--img">
                 <img src={item.src} alt="Placeholder" />
-              </div>
-              <div className="portfolio--section--card--content">
-                <div>
-                  <h3 className="portfolio--section--title">{item.title}</h3>
-                  <p className="text-md">{item.description}</p>
-                </div>
-                <p className="text-sm portfolio--link">
-                  {item.link}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 20 19"
-                    fill="none"
-                  >
-                    <path
-                      d="M4.66667 1.66675H18V15.0001M18 1.66675L2 17.6667L18 1.66675Z"
-                      stroke="currentColor"
-                      stroke-width="2.66667"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
-                </p>
-              </div>
-            </div>
+              </Box>
+              <Box className="portfolio--section--card--content">
+                <PoppinsEighteenText className="text-head-color">
+                  {item.title}
+                </PoppinsEighteenText>
+                <PoppinsForteenText className="text-color text-top">
+                  {item.description}
+                </PoppinsForteenText>
+                <Box className="project--card--link">
+                  <PoppinsSixteenText className="text-color portfolio--link">
+                    {item.github}
+                  </PoppinsSixteenText>
+                  <PoppinsSixteenText className="portfolio--link">
+                    {item.demo}
+                  </PoppinsSixteenText>
+                </Box>
+              </Box>
+            </Box>
           ))}
         </Box>
       </Box>
