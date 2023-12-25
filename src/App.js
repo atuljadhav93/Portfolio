31047e881435ecc72fd4c790c545f22de2519b31
Atul Jadhav <prathik.paip_ext@ltts.com>
2023-12-25 19:06:11 +0530

    1.0 router path changed

--- src/App.js
+++ src/App.js
@@ -7,16 +7,15 @@ import Home from "./content/index";
 function App() {
   return (
     <div className="App">
-      {/* <Router>
+      <Router>
         <div>
           <Navbar />
           <Routes>
-            <Route path="/" element={<Home />}></Route>
+            <Route path="/Portfolio" element={<Home />}></Route>
             <Route path="*" element={<div>404 Not Found</div>}></Route>
           </Routes>
         </div>
-      </Router> */}
-      <h1>Hi Ratnesh</h1>
+      </Router>
     </div>
   );
 }
