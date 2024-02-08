679f0f21c30a9904c43e9afa67e4fd4c67f758d3
Atul Jadhav <prathik.paip_ext@ltts.com>
2024-02-09 00:22:50 +0530

    [7] contact us form now functional

--- src/slice/ContactMe.js
+++ src/slice/ContactMe.js
@@ -2,12 +2,17 @@ import {
   createSlice
 } from '@reduxjs/toolkit';
 import emailjs from '@emailjs/browser';
+import {
+  serviceId,
+  templateId,
+  userId
+} from '../constants/const';
 
 const initialState = {
   contactMeData: {
     fullName: '',
     emailAddress: '',
-    phoneNumber: '', 
+    phoneNumber: '',
     emailSubject: '',
     message: '',
   },
@@ -36,39 +41,35 @@ const formSlice = createSlice({
       state.contactMeData[action.payload.field] = action.payload.value;
     },
     clearForm: (state) => {
-      state.contactMeData = {...initialState.contactMeData};
+      state.contactMeData = {
+        ...initialState.contactMeData
+      };
     }
   },
 });
 
 export const sendEmail = (data) => async (dispatch) => {
   try {
-    // Use your Email.js service ID, template ID, and user ID
-    const serviceId = 'service_0in2dp9';
-    const templateId = 'template_4rb7bmk';
-    const userId = '4v_iM7q6V2uFGq9nx';
-    //eslint-disable-next-line
-    console.log("check",data);
-    //eslint-disable-next-line
-    console.log("check1", data.emailAddress);
-    // Replace these placeholders with actual data
     const templateParams = {
-      to_subject: data.emailSubject,
-      to_name: data.fullName,
-      to_email: data.emailAddress,
-      to_phone: data.phoneNumber,
+      emailSubject: data.emailSubject,
+      fullName: data.fullName,
+      emailAddress: data.emailAddress,
+      phoneNumber: data.phoneNumber,
       message: data.message,
     };
-//esling-disable-next-line
-console.log("check2",templateParams);
     // Send email using Email.js
     await emailjs.send(serviceId, templateId, templateParams, userId);
 
     // Dispatch a success action if needed
-    dispatch({ type: 'SEND_EMAIL_SUCCESS' });
+    dispatch({
+      type: 'SEND_EMAIL_SUCCESS'
+    });
   } catch (error) {
     // Dispatch an error action if needed
-    dispatch({ type: 'SEND_EMAIL_ERROR', payload: error.message });
+    dispatch({
+      type: 'SEND_EMAIL_ERROR',
+      payload: error.message
+    });
   }
 };
 
